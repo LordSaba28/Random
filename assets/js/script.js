e15f93799fd343d93973d976475eb7609bf66180
LordSaba28 <123214954+LordSaba28@users.noreply.github.com>
2024-08-20 11:42:57 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const selectInput = document.querySelector('.Select-input');
+const selectButton = document.querySelector('.Select-button');
+const selectList = document.querySelector('.Select-list');
